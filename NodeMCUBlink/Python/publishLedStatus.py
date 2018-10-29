76da3e4e49f7e9969f3e36a292915c7304e4460d
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2018-10-29 07:52:21 +0100

    .

--- NodeMCUBlink/Python/publishLedStatus.py
+++ NodeMCUBlink/Python/publishLedStatus.py
@@ -5,16 +5,15 @@ import paho.mqtt.publish as publish
 import time
 
 host = "iot-raspi.local"
-
 x = 0
+
 while True:
-    print("Sending %s..." % x)
-    #publish.single("ledStatus", "%s" % x, hostname=host)
+    print("Sending %s ..." % x)
+
+    publish.single(
+        topic="ledStatus", 
+        payload="%s" % x, 
+        hostname=host)
+
     x = int(not x)
     time.sleep(1)
-
-    #print("Sending 1...")
-    #publish.single("ledStatus", "1", hostname=host)
-
-    #time.sleep(1)
-
